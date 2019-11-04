c6b76f1d4c3d761965dae856d98f74650f230bb7
Ronald Vega <ronald.vega48@gmail.com>
2019-11-04 16:48:48 -0600

    Using array of objects

--- LearningVue1/holaMundo.js
+++ LearningVue1/holaMundo.js
@@ -1,4 +1,12 @@
 const app = new Vue({
     //Aqui instanciamos cada uno de los containers, en este caso instanciamos el div llamado 'app'
-    el: '#app'
+    el: '#app',
+    data: {
+        titulo: "Hola Mundo con Vue",
+        temas: [
+            {unidad: "1 Hola Mundo", fecha: "07/10/19", estado:true},
+            {unidad: "2 Objetos en Vue", fecha: "10/11/19", estado:false},
+            {unidad: "3 Arreglos en Vue", fecha: "12/12/19", estado:false}
+        ]
+    }
 })
