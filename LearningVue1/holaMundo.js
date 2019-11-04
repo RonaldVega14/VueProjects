1122c3e06270f232a9584ee08f92011ad50637d7
Ronald Vega <ronald.vega48@gmail.com>
2019-11-04 17:17:26 -0600

    Adding button and events

--- LearningVue1/holaMundo.js
+++ LearningVue1/holaMundo.js
@@ -7,6 +7,12 @@ const app = new Vue({
             {unidad: "1 Hola Mundo", fecha: "07/10/19", estado:true},
             {unidad: "2 Objetos en Vue", fecha: "10/11/19", estado:false},
             {unidad: "3 Arreglos en Vue", fecha: "12/12/19", estado:false}
-        ]
+        ],
+        nuevoTema: '',
+    },
+    methods:{
+        agregarTema(){
+            console.log('Click!');
+        }
     }
 })
